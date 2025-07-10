b9df16c6e99be60ddc67c53fefc2e5e3b832c66e
IshimweArlene <ishimwearlene74@gmail.com>
2025-07-10 09:13:26 +0200

    added the signUp page and added styles

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,10 +1,9 @@
 'use client';
 import Image from 'next/image';
-import { FaUser, FaLock } from 'react-icons/fa';
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa';
+const Signup = () => {
+    return (
+        <div className="min-h-screen bg-gray-900 flex items-center justify-center">
       <div className="relative w-[320px] h-[600px] rounded-3xl overflow-hidden shadow-2xl">
         <div className="absolute w-full h-60 bg-green-400 skew-y-[10deg] origin-top-right z-0"></div>
 
@@ -18,24 +17,28 @@ export default function Home() {
               <input type="text" placeholder="USERNAME" className="w-full bg-transparent border-b border-gray-500 placeholder-white text-white text-sm text-center px-2 py-2 outline-none"/>
             </label>
           </div>
+          <div className="w-full mb-6">
+            <label className="flex items-center gap-2 text-white">
+              <FaEnvelope />
+              <input type="password" placeholder="EMAIL" className="w-full bg-transparent border-b border-gray-500 placeholder-white text-white text-sm text-center px-2 py-2 outline-none" />
+            </label>
+          </div>
 
           <div className="w-full mb-6">
             <label className="flex items-center gap-2 text-white">
               <FaLock />
-              <input
-                type="password"
-                placeholder="PASSWORD"
-                className="w-full bg-transparent border-b border-gray-500 placeholder-white text-white text-sm text-center px-2 py-2 outline-none"
-              />
+              <input type="password" placeholder="PASSWORD" className="w-full bg-transparent border-b border-gray-500 placeholder-white text-white text-sm text-center px-2 py-2 outline-none" />
             </label>
           </div>
-
-          <button className="w-full bg-white text-black py-2 rounded-full font-bold hover:bg-gray-200 transition-all">LOGIN</button>
+            
+          <button className="w-full bg-white text-black py-2 rounded-full font-bold hover:bg-gray-200 transition-all">Signup</button>
 
           <p className="text-sm text-white mt-4 cursor-pointer">forgot your password ?</p>
-          <button className="text-white mt-16 text-sm hover:text-blue-200">SIGN UP</button>
+          <button className="text-white mt-16 text-sm hover:text-blue-200">Login</button>
         </div>
       </div>
     </div>
-  );
+    );
 }
+ 
+export default Signup;
