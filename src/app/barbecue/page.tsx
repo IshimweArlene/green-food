e827d63298e7c76a4fbbfd3ec40d927c18f5fc5a
IshimweArlene <ishimwearlene74@gmail.com>
2025-07-13 22:01:53 +0200

    added another page

--- src/app/barbecue/page.tsx
+++ src/app/barbecue/page.tsx
@@ -1,16 +1,15 @@
-'use client';
 import TopBar from "../components/topBar";
 import SearchNav from "../components/searchBar";
 import MealCard from "../components/mealCard";
-import { meals } from "../meals/data";
-const Resto = () => {
+import {barbecue } from "../meals/barbecue";
+const Barbecue = () => {
     return (
         <div className="min-h-screen bg-gray-900 flex items-center justify-center">
             <div className="relative w-[320px] h-[600px] rounded-3xl overflow-hidden shadow-2xl bg-white">
               <TopBar />      
               <SearchNav />
-               <div className="px-4 py-2 overflow-y-auto h-[480px]">
-                {meals.map((meal, i) => (
+              <div className="px-4 py-2 overflow-y-auto h-[480px]">
+                {barbecue.map((meal, i) => (
                     <MealCard
                     key={i}
                     name={meal.name}
@@ -22,9 +21,7 @@ const Resto = () => {
               </div>
             </div>
         </div>
-            
-        
     );
 }
  
-export default Resto;
+export default Barbecue;
---
+++ src/app/meals/barbecue.ts
@@ -0,0 +1,50 @@
+export const barbecue = [
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+ {
+  name: "Master Grill",
+  rating: 4.4,
+  description: "Delicious ice cream with granola, fruit, and more!",
+  image: "/b1.png",
+ },
+]
