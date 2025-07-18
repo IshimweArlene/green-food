267ef6eac4ebeefaabb105644c96e3a0d10fa0ea
IshimweArlene <ishimwearlene74@gmail.com>
2025-07-18 06:58:22 +0200

    Added pharmacy page

---
+++ src/app/pharmacy/page.tsx
@@ -0,0 +1,13 @@
+import MealCard from "../components/mealCard";
+import TopBar from "../components/topBar";
+import { pharmacy } from '../meals/data';
+const Pharmacy = () => {
+    return (
+        <div>
+            <h1>Pharmacy Page</h1>
+            <p>Welcome to the pharmacy section of our application.</p>
+        </div>
+    );
+}
+ 
+export default Pharmacy;
